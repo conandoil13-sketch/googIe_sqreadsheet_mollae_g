da7c2443a0f2e6295d4576cec425795e2fcc2c85
임수호 <alltimesuho@imsuhoui-MacBookPro.local>
2026-05-03 02:09:47 +0900

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/mollaesheet-arcade/',
+  base: '/googIe_sqreadsheet_mollae_g/',
   plugins: [react()],
 });
